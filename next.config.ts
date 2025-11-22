755c376667e84a5ca632cdd68d864a316a3cd28f
wenby <wenby@satuplatform.com>
2025-11-22 21:03:52 +0700

    login dashboard signup

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,18 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+   async rewrites() {
+    return [
+      {
+        source: '/login',
+        destination: '/auth/login',
+      },
+       {
+        source: '/signup',
+        destination: '/auth/signup',
+      },
+    ]
+  },
 };
 
 export default nextConfig;
---
+++ src/components/forms/login-form.tsx
@@ -0,0 +1,125 @@
+"use client";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import Link from "next/link";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+
+export function LoginForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  const router=useRouter();
+  const loginSchema = z.object({
+    email: z.email({ message: "Invalid email address" }),
+    password: z
+      .string()
+      .min(6, { message: "Password must be at least 6 characters" }),
+  });
+
+  type LoginFormValues = z.infer<typeof loginSchema>;
+
+  interface LoginFormSubmitEvent {
+    ev: React.FormEvent<HTMLFormElement>;
+  }
+
+  const form = useForm<LoginFormValues>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = async (values: LoginFormValues) => {
+   await new Promise((r)=> setTimeout(() => {
+       r('done');
+    }, 500));
+    console.log("values", values);
+    router.push('/dashboard');
+  };
+  return (
+    <div className={cn("flex flex-col gap-6 ", className)} {...props}>
+      <Card>
+        <CardHeader>
+          <CardTitle>Login to your account</CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <FieldGroup>
+              <Field>
+                <FieldLabel htmlFor="email">Email</FieldLabel>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  {...form.register("email")}
+                  required
+                />
+                {form.formState.errors.email && (
+                  <p className="text-red-500 text-sm">
+                    {form.formState.errors.email.message}
+                  </p>
+                )}
+              </Field>
+              <Field>
+                <div className="flex items-center">
+                  <FieldLabel htmlFor="password">Password</FieldLabel>
+                  {/* <a
+                    href="#"
+                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                  >
+                    Forgot your password?
+                  </a> */}
+                </div>
+                <Input
+                  id="password"
+                  {...form.register("password")}
+                  type="password"
+                  required
+                />
+                {form.formState.errors.password && (
+                  <p className="text-red-500 text-sm">
+                    {form.formState.errors.password.message}
+                  </p>
+                )}
+              </Field>
+              <Field>
+                <Button type="submit">
+                  {" "}
+                  {form.formState.isSubmitting ? "Logging in..." : "Login"}
+                </Button>
+                {/* <Button variant="outline" type="button">
+                  Login with Google
+                </Button> */}
+                <FieldDescription className="text-center">
+                  Don&apos;t have an account?{" "}
+                  <Link href="/signup">Sign up</Link>
+                </FieldDescription>
+              </Field>
+            </FieldGroup>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
